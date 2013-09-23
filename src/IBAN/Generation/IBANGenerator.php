fc733b27ee506568ab5c23e604bad774797be7a7
Jan Schädlich <schaedlich.jan@gmail.com>
2013-09-23 17:15:51 +0200

    Extracted method for argument validation

--- src/IBAN/Generation/IBANGenerator.php
+++ src/IBAN/Generation/IBANGenerator.php
@@ -11,18 +11,24 @@ class IBANGenerator
     }
     
     public function generate($localeCode, $instituteIdentification, $bankAccountNumber) {
-        if (empty($localeCode)) {
-            throw new \InvalidArgumentException('localeCode is missing');
-        } else if (empty($instituteIdentification)) {
-            throw new \InvalidArgumentException('instituteIdentification is missing');
-        } else if (empty($bankAccountNumber)) {
-            throw new \InvalidArgumentException('bankAccountNumber is missing');
-        } else if (empty(\IBAN\Core\Constants::$ibanFormatMap[$localeCode])) {
-            throw new \InvalidArgumentException('localeCode not exists');
-        } else {
-            $iban = new \IBAN\Core\IBAN('');
+        if ($this->areArgumentsValid($localeCode, $instituteIdentification, $bankAccountNumber)) {
             $ibanRule = $this->ibanRuleFactory->createIBANRule($localeCode, $instituteIdentification);
-            return $iban->generate($ibanRule, $bankAccountNumber);
+            return $ibanRule->generateIban($bankAccountNumber);
         }
+    }
+    
+    private function areArgumentsValid($localeCode, $instituteIdentification, $bankAccountNumber) {
+    	if (empty($localeCode)) {
+    		throw new \InvalidArgumentException('localeCode is missing');
+    	} else if (empty($instituteIdentification)) {
+    		throw new \InvalidArgumentException('instituteIdentification is missing');
+    	} else if (empty($bankAccountNumber)) {
+    		throw new \InvalidArgumentException('bankAccountNumber is missing');
+    	} else if (empty(\IBAN\Core\Constants::$ibanFormatMap[$localeCode])) {
+    		throw new \InvalidArgumentException('localeCode not exists');
+    	} else {
+    		return true;
+    	}
+    	return false;
     }
 }
