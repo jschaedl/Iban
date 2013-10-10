a0ba0eeb982d414d087f9d6ac5f940422a1442c5
Jan Schaedlich <schaedlich.jan@gmail.com>
2013-10-10 13:31:53 +0200

    Refactored Generator

--- src/IBAN/Generation/IBANGenerator.php
+++ src/IBAN/Generation/IBANGenerator.php
@@ -2,32 +2,52 @@
 
 namespace IBAN\Generation;
 
+use IBAN\Rule\IBANRuleFactory;
+
 class IBANGenerator
 {
     private $ibanRuleFactory;
     
-    public function __construct() {
-        $this->ibanRuleFactory = new \IBAN\Rule\IBANRuleFactory();
+    private $localeCode;
+    private $instituteIdentification;
+    private $bankAccountNumber;
+    
+    public function __construct($ibanRuleFactoryClassName = '\IBAN\Rule\IBANRuleFactory') {
+        $this->ibanRuleFactory = new $ibanRuleFactoryClassName();
     }
     
     public function generate($localeCode, $instituteIdentification, $bankAccountNumber) {
-        $ibanRule = null;
-        $iban = null;
-        if ($this->areArgumentsValid($localeCode, $instituteIdentification, $bankAccountNumber)) {
-            $ibanRule = $this->ibanRuleFactory->createIBANRule($localeCode, $instituteIdentification, $bankAccountNumber);
-            $iban = $ibanRule->generateIban();
+        $this->localeCode = $localeCode;
+        $this->instituteIdentification = $instituteIdentification;
+        $this->bankAccountNumber = $bankAccountNumber;
+        
+        if ($this->areArgumentsValid()) {
+            $ibanRule = $this->createRule();
+            $iban = $this->createIban($ibanRule);
         }
+        
         return $iban;
     }
     
-    private function areArgumentsValid($localeCode, $instituteIdentification, $bankAccountNumber) {
-    	if (empty($localeCode)) {
+    private function createRule() {
+    	return $this->ibanRuleFactory->createIBANRule(
+	        $this->localeCode, 
+	        $this->instituteIdentification, 
+	        $this->bankAccountNumber);
+    }
+    
+    private function createIban($ibanRule) {
+        return $ibanRule->generateIban();
+    }
+    
+    private function areArgumentsValid() {
+    	if (empty($this->localeCode)) {
     		throw new \InvalidArgumentException('localeCode is missing');
-    	} else if (empty($instituteIdentification)) {
+    	} else if (empty($this->instituteIdentification)) {
     		throw new \InvalidArgumentException('instituteIdentification is missing');
-    	} else if (empty($bankAccountNumber)) {
+    	} else if (empty($this->bankAccountNumber)) {
     		throw new \InvalidArgumentException('bankAccountNumber is missing');
-    	} else if (empty(\IBAN\Core\Constants::$ibanFormatMap[$localeCode])) {
+    	} else if (empty(\IBAN\Core\Constants::$ibanFormatMap[$this->localeCode])) {
     		throw new \InvalidArgumentException('localeCode not exists');
     	} else {
     		return true;
