1b1358380ea8b5efe2fcae2fb00f4405f5e35ecb
Jan Schädlich <schaedlich.jan@gmail.com>
2013-10-27 13:55:58 +0100

    Inlined base class rule methods

--- src/IBAN/Rule/DE/Rule000000.php
+++ src/IBAN/Rule/DE/Rule000000.php
@@ -6,30 +6,66 @@ use IBAN\Rule\IBANRule;
 
 class Rule000000 extends IBANRule
 {   
+	public function __construct($localeCode, $instituteIdentification, $bankAccountNumber) {
+		$this->localeCode = $localeCode;
+		$this->instituteIdentification = $instituteIdentification;
+		$this->bankAccountNumber = $bankAccountNumber;
+	}
+	
     public function generateIban() {        
         $invertedIban = $this->getInvertedIban();
         $numericRepresentationOfInvertedIban = $this->getNumericRepresentation($invertedIban);
         $checksum = $this->generateChecksum($numericRepresentationOfInvertedIban);
-        return $this->localeCode . $checksum . $this->normalizedInstituteIdentification . $this->normalizedBankAccountNumber;
+        return $this->localeCode . $checksum . $this->normalizeInstituteIdentification() . 
+        	$this->normalizeBankAccountNumber();
     }
     
-    public function getLocalCodeNormalizePrefix() {
+    protected function getNumericRepresentation($letterRepresentation) {
+    	$numericRepresentation = '';
+    	foreach (str_split($letterRepresentation) as $char) {
+    		if (array_search($char, \IBAN\Core\Constants::$letterMapping)) {
+    			$numericRepresentation .= array_search($char, \IBAN\Core\Constants::$letterMapping) + 9;
+    		} else {
+    			$numericRepresentation .= $char;
+    		}
+    	}
+    	return $numericRepresentation;
+    }
+    
+    protected function generateChecksum($numericRepresentationOfInvertedIban) {
+    	$modResult = bcmod($numericRepresentationOfInvertedIban, 97);
+    	$checksum = 98 - $modResult;
+    	if ($checksum < 10) {
+    		$checksum = '0' . $checksum;
+    	}
+    	return $checksum;
+    }
+    
+    protected function instituteIdentificationEquals($instituteIdentification) {
+    	return strcmp($this->instituteIdentification, 
+    		$instituteIdentification) == 0;
+    }
+    
+    protected function bankAccountNumberEquals($bankAccountNumber) {
+    	return strcmp($this->bankAccountNumber, 
+    		$bankAccountNumber) == 0;
+    }
+    
+    protected function getLocalCodeNormalizePrefix() {
         return '00';
     }
     
-    public function getInstituteIdentificationLength() {
+    protected function getInstituteIdentificationLength() {
         return 8;
     }
     
-    public function getBankAccountNumberLength() {
+    protected function getBankAccountNumberLength() {
         return 10;
     }
     
     private function getInvertedIban() {
-        $this->normalizedLocalCode = $this->normalizeLocaleCode();
-        $this->normalizedInstituteIdentification = $this->normalizeInstituteIdentification();
-        $this->normalizedBankAccountNumber = $this->normalizeBankAccountNumber();
-        return $this->normalizedInstituteIdentification . $this->normalizedBankAccountNumber . $this->normalizedLocalCode;
+        return $this->normalizeInstituteIdentification() . 
+        	$this->normalizeBankAccountNumber() . $this->normalizeLocaleCode();
     }
     
     private function normalizeLocaleCode() {
@@ -37,10 +73,12 @@ class Rule000000 extends IBANRule
     }
     
     private function normalizeInstituteIdentification() {
-        return str_pad($this->instituteIdentification, $this->getInstituteIdentificationLength(), '0', STR_PAD_LEFT);
+        return str_pad($this->instituteIdentification, 
+        	$this->getInstituteIdentificationLength(), '0', STR_PAD_LEFT);
     }
     
     private function normalizeBankAccountNumber() {
-        return str_pad($this->bankAccountNumber, $this->getBankAccountNumberLength(), '0', STR_PAD_LEFT);
+        return str_pad($this->bankAccountNumber, 
+        	$this->getBankAccountNumberLength(), '0', STR_PAD_LEFT);
     }
 }
--- src/IBAN/Rule/IBANRule.php
+++ src/IBAN/Rule/IBANRule.php
@@ -3,55 +3,6 @@
 namespace IBAN\Rule;
 
 abstract class IBANRule
-{       
-    protected $localeCode;
-    protected $instituteIdentification;
-    protected $bankAccountNumber;
-    
-    protected $normalizedLocalCode;
-    protected $normalizedInstituteIdentification;
-    protected $normalizedBankAccountNumber;
-    
-    public function __construct($localeCode, $instituteIdentification, $bankAccountNumber) {
-        $this->localeCode = $localeCode;
-        $this->instituteIdentification = $instituteIdentification;
-        $this->bankAccountNumber = $bankAccountNumber;
-    }
-    
-    protected function getNumericRepresentation($letterRepresentation) {
-        $numericRepresentation = '';
-        foreach (str_split($letterRepresentation) as $char) {
-            if (array_search($char, \IBAN\Core\Constants::$letterMapping)) {
-                $numericRepresentation .= array_search($char, \IBAN\Core\Constants::$letterMapping) + 9;
-            } else {
-                $numericRepresentation .= $char;
-            }
-        }
-        return $numericRepresentation;
-    }
-    
-    protected function generateChecksum($numericRepresentationOfInvertedIban) {
-        $modResult = bcmod($numericRepresentationOfInvertedIban, 97);
-        $checksum = 98 - $modResult;
-        if ($checksum < 10) {
-            $checksum = '0' . $checksum;
-        }
-        return $checksum;
-    }
-    
-    protected function instituteIdentificationEquals($instituteIdentification) {
-        return strcmp($this->instituteIdentification, $instituteIdentification) == 0;
-    }
-    
-    protected function bankAccountNumberEquals($bankAccountNumber) {
-        return strcmp($this->bankAccountNumber, $bankAccountNumber) == 0;
-    }
-    
+{   
     abstract public function generateIban();
-    
-    abstract public function getLocalCodeNormalizePrefix();
-    
-    abstract public function getInstituteIdentificationLength();
-    
-    abstract public function getbankAccountNumberLength();
 }
