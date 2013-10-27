0ad5a2a19cb2eff5cee0125da0946d483c2b8d19
Jan Schädlich <schaedlich.jan@gmail.com>
2013-10-27 13:13:56 +0100

    Enhanced script for getting more information out of bankleitzahlen datei

--- script/format_ibans.py
+++ script/format_ibans.py
@@ -14,7 +14,22 @@ def main():
 		content = ibans.readlines()
 
 	for line in content:
-		print '"' + (line[:8]).strip() + '" => "' + (line[9:16]).strip() + '", ', 
+		string = ''
+		for i in line:
+			string += str(i)
+		string_splitted = string.split(';')
+		instituteIdentification = string_splitted[0]
+		bankname = string_splitted[2]
+		bic = string_splitted[7]
+		rule = string_splitted[13]
+
+		array_string = 'array('
+		array_string += '"bankname" => "' + bankname.strip() + '", ' 
+		array_string += '"bic" => "' + bic.strip() + '", ' 
+		array_string += '"rule" => "' + rule.strip() + '", ' 
+		array_string += ')'
+
+		print '"' + instituteIdentification.strip() + '" => ' + array_string + ', ', 
 	print ");"
 
 
