843e40ac15ba8394fc5bbe27794aac9819937621
Jan Schaedlich <schaedlich.jan@gmail.com>
2013-10-28 12:02:07 +0100

    Added bank account number substitutions (e9222f1)

--- src/IBAN/Rule/DE/Rule002300.php
+++ src/IBAN/Rule/DE/Rule002300.php
@@ -4,10 +4,7 @@ namespace IBAN\Rule\DE;
 
 class Rule002300 extends \IBAN\Rule\DE\Rule000000
 {    
-	public function generateIban() {
-		if ($this->bankAccountNumberEquals('700')) {
-			$this->bankAccountNumber = '1000700800';
-		}
-        return parent::generateIban();
-    }
+    protected $bankAccountSubstitutions = array(
+        "700" => "1000700800"
+    );
 }
