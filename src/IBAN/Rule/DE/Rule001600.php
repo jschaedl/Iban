a660a294f8b3e8e886a525288ab22ef36909a292
Jan Schaedlich <schaedlich.jan@gmail.com>
2013-10-28 11:53:29 +0100

    Fixed formatting

--- src/IBAN/Rule/DE/Rule001600.php
+++ src/IBAN/Rule/DE/Rule001600.php
@@ -5,6 +5,6 @@ namespace IBAN\Rule\DE;
 class Rule001600 extends \IBAN\Rule\DE\Rule000000
 {    
     protected $bankAccountSubstitutions = array(
-            "300000" => "18128012"
+        "300000" => "18128012"
     );
 }
