320dff585373c5ab3711a594a385fd407d8cfa98
Jan Schaedlich <schaedlich.jan@gmail.com>
2013-10-28 11:51:13 +0100

    Added bank account number substitutions

--- src/IBAN/Rule/DE/Rule001500.php
+++ src/IBAN/Rule/DE/Rule001500.php
@@ -4,31 +4,21 @@ namespace IBAN\Rule\DE;
 
 class Rule001500 extends \IBAN\Rule\DE\Rule000000
 {    
-	public function generateIban() {
-        $this->instituteIdentification = '37060193';
-        if ($this->bankAccountNumberEquals('556')) {
-            $this->bankAccountNumber = '0000101010';
-        } else if ($this->bankAccountNumberEquals('888')) {
-            $this->bankAccountNumber = '0031870011';
-        } else if ($this->bankAccountNumberEquals('4040')) {
-            $this->bankAccountNumber = '4003600101';
-        } else if ($this->bankAccountNumberEquals('5826')) {
-            $this->bankAccountNumber = '1015826017';
-        } else if ($this->bankAccountNumberEquals('25000')) {
-            $this->bankAccountNumber = '0025000110';
-        } else if ($this->bankAccountNumberEquals('393393')) {
-            $this->bankAccountNumber = '0033013019';
-        } else if ($this->bankAccountNumberEquals('444555')) {
-            $this->bankAccountNumber = '0032230016';
-        } else if ($this->bankAccountNumberEquals('603060')) {
-            $this->bankAccountNumber = '6002919018';
-        } else if ($this->bankAccountNumberEquals('2120041')) {
-            $this->bankAccountNumber = '0002130041';
-        } else if ($this->bankAccountNumberEquals('80868086')) {
-            $this->bankAccountNumber = '4007375013';
-        } else if ($this->bankAccountNumberEquals('400569017')) {
-            $this->bankAccountNumber = '4000569017';
-        }
-        return parent::generateIban();
-    }
+    public function __construct($localeCode, $instituteIdentification, $bankAccountNumber) {
+		parent::__construct($localeCode, '37060193', $bankAccountNumber);
+	}
+    
+    protected $bankAccountSubstitutions = array(
+            "556" =>    "0000101010"
+            , "888" => "0031870011"
+            , "4040" => "4003600101"
+            , "5826" => "1015826017"
+            , "25000" => "0025000110"
+            , "393393" => "0033013019"
+            , "444555" => "0032230016"
+            , "603060" => "6002919018"
+            , "2120041" => "0002130041"
+            , "80868086" => "4007375013"
+            , "400569017" => "4000569017"
+    );
 }
