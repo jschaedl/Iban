25df7fad44a2f9ab37693634f29673985e375843
Jan Schaedlich <schaedlich.jan@gmail.com>
2013-10-28 11:51:56 +0100

    Added bank account number substitutions (320dff5)

--- src/IBAN/Rule/DE/Rule001600.php
+++ src/IBAN/Rule/DE/Rule001600.php
@@ -4,10 +4,7 @@ namespace IBAN\Rule\DE;
 
 class Rule001600 extends \IBAN\Rule\DE\Rule000000
 {    
-	public function generateIban() {
-        if ($this->bankAccountNumberEquals('300000')) {
-            $this->bankAccountNumber = '18128012';
-        } 
-        return parent::generateIban();
-    }
+    protected $bankAccountSubstitutions = array(
+            "300000" => "18128012"
+    );
 }
