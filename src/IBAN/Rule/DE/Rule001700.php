4d543f82a3bf8d185061f0d33e29c536c60a05eb
Jan Schaedlich <schaedlich.jan@gmail.com>
2013-10-28 11:57:48 +0100

    Added bank account number substitutions

--- src/IBAN/Rule/DE/Rule001700.php
+++ src/IBAN/Rule/DE/Rule001700.php
@@ -4,28 +4,16 @@ namespace IBAN\Rule\DE;
 
 class Rule001700 extends \IBAN\Rule\DE\Rule000000
 {    
-	public function generateIban() {
-        if ($this->bankAccountNumberEquals('100')) {
-            $this->bankAccountNumber = '2009090013';
-        } else if ($this->bankAccountNumberEquals('111')) {
-            $this->bankAccountNumber = '2111111017';
-        } else if ($this->bankAccountNumberEquals('240')) {
-            $this->bankAccountNumber = '2100240010';
-        } else if ($this->bankAccountNumberEquals('4004')) {
-            $this->bankAccountNumber = '2204004016';
-        } else if ($this->bankAccountNumberEquals('4444')) {
-            $this->bankAccountNumber = '2044444014';
-        } else if ($this->bankAccountNumberEquals('6060')) {
-            $this->bankAccountNumber = '2016060014';
-        } else if ($this->bankAccountNumberEquals('102030')) {
-            $this->bankAccountNumber = '1102030016';
-        } else if ($this->bankAccountNumberEquals('333333')) {
-            $this->bankAccountNumber = '2033333016';
-        } else if ($this->bankAccountNumberEquals('909090')) {
-            $this->bankAccountNumber = '2009090013';
-        } else if ($this->bankAccountNumberEquals('50005000')) {
-            $this->bankAccountNumber = '5000500013';
-        }
-        return parent::generateIban();
-    }
+    protected $bankAccountSubstitutions = array(
+        "100" => "2009090013"
+        , "111" => "2111111017"
+        , "240" => "2100240010"
+        , "4004" => "2204004016"
+        , "4444" => "2044444014"
+        , "6060" => "2016060014"
+        , "102030" => "1102030016"
+        , "333333" => "2033333016"
+        , "909090" => "2009090013"
+        , "50005000" => "5000500013"
+    );
 }
