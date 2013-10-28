ad8373e8b5fe9d322450bbfcf204c27cfa83a9f6
Jan Schaedlich <schaedlich.jan@gmail.com>
2013-10-28 11:41:02 +0100

    Added  banbk account substitutions

--- src/IBAN/Rule/DE/Rule005100.php
+++ src/IBAN/Rule/DE/Rule005100.php
@@ -4,16 +4,10 @@ namespace IBAN\Rule\DE;
 
 class Rule005100 extends \IBAN\Rule\DE\Rule000000
 {    
-	public function generateIban() {
-		if ($this->bankAccountNumberEquals('333')) {
-			$this->bankAccountNumber = '7832500881';
-		} else if ($this->bankAccountNumberEquals('502')) {
-			$this->bankAccountNumber = '1108884';
-		} else if ($this->bankAccountNumberEquals('500500500')) {
-			$this->bankAccountNumber = '5005000';
-		} else if ($this->bankAccountNumberEquals('502502502')) {
-			$this->bankAccountNumber = '1108884';
-		}
-        return parent::generateIban();
-    }
+    protected $bankAccountSubstitutions = array(
+            "333" => "7832500881"
+            , "502" => "1108884"
+            , "500500500" => "5005000"
+            , "502502502" => "1108884"
+    );
 }
