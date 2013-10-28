4b41bf000464290c8f017fde85ededb4cfd00763
Jan Schaedlich <schaedlich.jan@gmail.com>
2013-10-28 11:47:16 +0100

    Added bank account number substitutions

--- src/IBAN/Rule/DE/Rule001100.php
+++ src/IBAN/Rule/DE/Rule001100.php
@@ -4,12 +4,8 @@ namespace IBAN\Rule\DE;
 
 class Rule001100 extends \IBAN\Rule\DE\Rule000000
 {    
-	public function generateIban() {
-        if ($this->bankAccountNumberEquals('1000')) {
-            $this->bankAccountNumber = '8010001';
-        } else if ($this->bankAccountNumberEquals('47800')) {
-            $this->bankAccountNumber = '47803';
-        } 
-        return parent::generateIban();
-    }
+    protected $bankAccountSubstitutions = array(
+        "1000" =>    "8010001"
+        , "47800" => "47803"
+    );
 }
