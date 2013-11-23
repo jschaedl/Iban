323d5d36e46ffb37f5ea51d39116083e8ab3c816
Jan Schädlich <schaedlich.jan@gmail.com>
2013-11-23 16:38:20 +0100

    Added new rule data from deutsche bundesbank

--- script/format_ibans.py
+++ script/format_ibans.py
@@ -17,29 +17,33 @@ checksumMethod = ''
 rule = ''
 
 def printProperties(string_splitted):
-	bankName = string_splitted[2]
-	bankPoCode = string_splitted[3]
-	bankCity = string_splitted[4]
-	bankShortname = string_splitted[5]
-	pan = string_splitted[6]
-	bic = string_splitted[7]
-	checksumMethod = string_splitted[8]
-	rule = string_splitted[13]
+	bankName = string_splitted[2].strip().replace('"', '')
+	bankPoCode = string_splitted[3].strip().replace('"', '')
+	bankCity = string_splitted[4].strip().replace('"', '')
+	bankShortname = string_splitted[5].strip().replace('"', '')
+	pan = string_splitted[6].strip().replace('"', '')
+	bic = string_splitted[7].strip().replace('"', '')
+	checksumMethod = string_splitted[8].strip().replace('"', '')
+	succession = string_splitted[12].strip().replace('"', '')
+	rule = string_splitted[13].strip().replace('"', '')
+
 	array_string = 'array('
-	array_string += '"bankName" => "' + bankName.strip() + '", ' 
-	array_string += '"bankPoCode" => "' + bankPoCode.strip() + '", ' 
-	array_string += '"bankCity" => "' + bankCity.strip() + '", ' 
-	array_string += '"bankShortname" => "' + bankShortname.strip() + '", ' 
-	array_string += '"pan" => "' + pan.strip() + '", ' 
-	array_string += '"bic" => "' + bic.strip() + '", ' 
-	array_string += '"checksumMethod" => "' + checksumMethod.strip() + '", ' 
-	array_string += '"rule" => "' + rule.strip() + '", ' 
+	array_string += '"bankName" => "' + bankName + '", ' 
+	array_string += '"bankPoCode" => "' + bankPoCode + '", ' 
+	array_string += '"bankCity" => "' + bankCity + '", ' 
+	array_string += '"bankShortname" => "' + bankShortname + '", ' 
+	array_string += '"pan" => "' + pan + '", ' 
+	array_string += '"bic" => "' + bic + '", ' 
+	array_string += '"checksumMethod" => "' + checksumMethod + '", ' 
+	array_string += '"succession" => "' + succession + '", ' 
+	array_string += '"rule" => "' + rule + '"' 
 	array_string += ')'
+
 	return array_string
 
 def main():
-	print "<?php"
-	print "return array(", 
+	print '<?php'
+	print 'return array(', 
 	
 	with open(sys.argv[1], 'rb') as ibans:
 		content = ibans.readlines()
@@ -49,9 +53,9 @@ def main():
 		for i in line:
 			string += str(i)
 		string_splitted = string.split(';')
-		instituteIdentification = string_splitted[0]
+		instituteIdentification = string_splitted[0].strip().replace('"', '')
 		array_string = printProperties(string_splitted)
-		print '"' + instituteIdentification.strip() + '" => ' + array_string + ', ', 
+		print '"' + instituteIdentification + '" => ' + array_string + ', ', 
 	print ");"
 
 
