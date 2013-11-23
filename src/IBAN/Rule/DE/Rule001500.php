6cc44824f78afdd00e8640e32b4400cf575fcc18
Jan Schädlich <schaedlich.jan@gmail.com>
2013-11-23 16:42:32 +0100

    Added license information

--- src/IBAN/Rule/DE/Rule001500.php
+++ src/IBAN/Rule/DE/Rule001500.php
@@ -1,5 +1,13 @@
 <?php
-
+/**
+ * Iban
+ *
+ * @author      Jan Schaedlich <schaedlich.jan@gmail.com>
+ * @copyright   2013 Jan Schaedlich
+ * @link        https://github.com/jschaedl/Iban
+ *
+ * MIT LICENSE
+ */
 namespace IBAN\Rule\DE;
 
 class Rule001500 extends \IBAN\Rule\DE\Rule000000
