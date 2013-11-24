9e00c3329e00712d55c0a6783ff33f9427fdccf4
Jan Schädlich <schaedlich.jan@gmail.com>
2013-11-24 12:32:06 +0100

    Fixed exception message

--- src/IBAN/Rule/DE/Rule000100.php
+++ src/IBAN/Rule/DE/Rule000100.php
@@ -13,6 +13,6 @@ namespace IBAN\Rule\DE;
 class Rule000100 extends \IBAN\Rule\DE\Rule000000
 {
     public function generateIban() {
-        throw new \Exception('this rule is not used');
+        throw new \Exception('no iban generation');
     }
 }
