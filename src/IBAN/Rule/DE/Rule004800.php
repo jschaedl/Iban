4b19b5d20a21d1b75448fa37032b3e7bdb85956a
Jan Schädlich <schaedlich.jan@gmail.com>
2013-11-24 09:38:37 +0100

    Fixed license information

--- src/IBAN/Rule/DE/Rule004800.php
+++ src/IBAN/Rule/DE/Rule004800.php
@@ -2,7 +2,8 @@
 /**
  * Iban
  *
- * @author      Stefan Warnat
+ * @author      Jan Schaedlich <schaedlich.jan@gmail.com>
+ * @copyright   2013 Jan Schaedlich
  * @link        https://github.com/jschaedl/Iban
  *
  * MIT LICENSE
