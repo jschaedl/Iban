a52dccf76df2578d5be4ff9df26964212ff98f20
Jan Schädlich <schaedlich.jan@gmail.com>
2013-12-04 20:57:30 +0100

    Added rule 001001

---
+++ library/IBAN/Rule/DE/Rule001001.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Iban
+ *
+ * @author      Jan Schaedlich <schaedlich.jan@gmail.com>
+ * @copyright   2013 Jan Schaedlich
+ * @link        https://github.com/jschaedl/Iban
+ *
+ * MIT LICENSE
+ */
+namespace IBAN\Rule\DE;
+
+class Rule001001 extends \IBAN\Rule\DE\Rule001000
+{    
+	protected $instituteIdentificationSubstitutions = array(
+		"50050222" => "50050201"
+	);
+	
+    public function generateIban() {
+        if ($this->instituteIdentificationEquals('50050201') && 
+        	$this->bankAccountNumberEquals('2000')) {
+            return 'DE42500502010000222000';
+        } else if ($this->instituteIdentificationEquals('50050201') && 
+        	$this->bankAccountNumberEquals('800000')) {
+            return 'DE89500502010000180802';
+        } else {
+            return parent::generateIban();
+        }
+    }
+}
