c41ffa4c0226369736f6b3fbf6f81c3104d78311
Jan Schädlich <schaedlich.jan@gmail.com>
2013-12-04 21:51:57 +0100

    Added rules 000502, 002002, 005500, 005600 and removed 00501, 002001

---
+++ library/IBAN/Rule/DE/Rule005500.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Iban
+ *
+ * @author      Jan Schaedlich <schaedlich.jan@gmail.com>
+ * @copyright   2013 Jan Schaedlich
+ * @link        https://github.com/jschaedl/Iban
+ *
+ * MIT LICENSE
+ */
+namespace IBAN\Rule\DE;
+
+class Rule005500 extends \IBAN\Rule\DE\Rule000000 
+{
+	public function __construct($localeCode, $instituteIdentification, $bankAccountNumber) {
+		parent::__construct($localeCode, '25410200', $bankAccountNumber);
+	}	
+}
