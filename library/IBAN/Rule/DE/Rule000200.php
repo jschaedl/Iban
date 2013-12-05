ed222679632d6e6be64efe807d11ae902465b13b
Jan Schaedlich <schaedlich.jan@gmail.com>
2013-12-05 12:42:23 +0100

    fixed rule 00200

--- library/IBAN/Rule/DE/Rule000200.php
+++ library/IBAN/Rule/DE/Rule000200.php
@@ -13,8 +13,8 @@ namespace IBAN\Rule\DE;
 class Rule000200 extends \IBAN\Rule\DE\Rule000000
 {
     public function generateIban() {
-    	if (preg_match('/' . '[0-9A-Z]{7}[8]{1}[6]{1}[0-9A-Z]{1}' . '/', $this->bankAccountNumber) === 1 || 
-            preg_match('/' . '[0-9A-Z]{7}[6]{1}[0-9A-Z]{2}' . '/', $this->bankAccountNumber) === 1) {
+    	if (preg_match('/' . '[0-9]{7}[8]{1}[6]{1}[0-9]{1}' . '/', $this->bankAccountNumber) === 1 || 
+            preg_match('/' . '[0-9]{7}[6]{1}[0-9]{2}' . '/', $this->bankAccountNumber) === 1) {
     		return '';
         } else { 
             return parent::generateIban();
