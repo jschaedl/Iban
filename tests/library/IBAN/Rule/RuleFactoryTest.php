5c10a6ea4504a22a452078d2e8ea3fe2ce01d00a
Jan Schädlich <schaedlich.jan@gmail.com>
2013-12-10 23:17:44 +0100

    Fixed indentation

--- tests/library/IBAN/Rule/RuleFactoryTest.php
+++ tests/library/IBAN/Rule/RuleFactoryTest.php
@@ -5,7 +5,7 @@ use IBAN\Rule\RuleFactory;
 
 class RuleFactoryTest extends \PHPUnit_Framework_TestCase
 {
- public function testCreateDE()
+ 	public function testCreateDE()
     {
         $this->assertInstanceOf('IBAN\Rule\RuleFactory', RuleFactory::DE());
     }
